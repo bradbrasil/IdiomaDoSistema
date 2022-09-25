2fb63392340f1f56657fd00280cead80127e73cc
bradbrasil <bradbrasil13@gmail.com>
2022-09-25 15:12:41 +0100

    Initial commit

---
+++ src/main/java/idiomadosistema/IdiomaDoSistema/IdiomaDoSistema.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package idiomadosistema.IdiomaDoSistema;
+
+import java.util.Locale;
+
+
+
+/**
+ *
+ * @author bradb
+ */
+public class IdiomaDoSistema {
+
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.println("O idioma do Sistema é: ");
+        System.out.println(loc.getDisplayLanguage());
+        
+    }
+}   
+       
+
+
